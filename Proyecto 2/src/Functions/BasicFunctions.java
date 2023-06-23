b44ba363bae8cb928435b75565c469177652bb6c
María <mcuervo@correo.unimet.edu.ve>
2023-06-22 21:39:12 -0400

    Todo

--- Proyecto 2/src/Functions/BasicFunctions.java
+++ Proyecto 2/src/Functions/BasicFunctions.java
@@ -4,10 +4,91 @@
  */
 package Functions;
 
+import com.csvreader.CsvReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Anabella Jaua
  */
 public class BasicFunctions {
+    public void Reservas(){
+         List<Reservas> usuarios = new ArrayList<Reservas>(); // Lista donde guardaremos los datos del archivo
+        
+        try{
+            
+            
+            CsvReader leerUsuarios = new CsvReader("C:\\Users\\hp\\Documents\\CSV\\Booking_hotel - reservas.csv");
+            leerUsuarios.readHeaders();
+            
+            // Mientras haya lineas obtenemos los datos del archivo
+            while(leerUsuarios.readRecord()) {
+                String ci = leerUsuarios.get(0);
+                String f_name = leerUsuarios.get(1);
+                String l_name = leerUsuarios.get(2);
+                String email = leerUsuarios.get(3);
+                String genero = leerUsuarios.get(4);
+                String tipo_hab = leerUsuarios.get(4);
+                String celular = leerUsuarios.get(6);
+                
+                usuarios.add(new Reservas(ci, f_name, l_name, email, genero, tipo_hab, celular)); // Añade la informacion a la lista
+            }
+            
+            leerUsuarios.close(); // Cierra el archivo
+            
+            System.out.println("Datos de la reserva: ");
+            // Recorremos la lista y la mostramos en la pantalla
+            for(Reservas user : usuarios) {
+                System.out.println(user.getCi() + " , "
+                    + user.getF_name() + " , "
+                    + user.getL_name() + " , "
+                    + user.getEmail() + " , "
+                    + user.getGenero() + " , "
+                    + user.getTipo_hab() + " , "
+                    +user.getCelular());
+            }
+            
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
     
+    public void Habitaciones(){
+        try{
+            List<Habitaciones> hab = new ArrayList<Habitaciones>(); // Lista donde guardaremos los datos del archivo
+            
+            CsvReader leerhabitaciones = new CsvReader("C:\\Users\\hp\\Documents\\CSV\\Booking_hotel - habitaciones.csv");
+            leerhabitaciones.readHeaders();
+            
+            // Mientras haya lineas obtenemos los datos del archivo
+            while(leerhabitaciones.readRecord()) {
+                String num_hab = leerhabitaciones.get(0);
+                String tipo_hab = leerhabitaciones.get(1);
+                String piso = leerhabitaciones.get(2);
+                
+                hab.add(new Habitaciones(num_hab, tipo_hab, piso)); // Añade la informacion a la lista
+            }
+            
+            leerhabitaciones.close(); // Cierra el archivo
+            
+            System.out.println("Datos de las habitaciones: ");
+            // Recorremos la lista y la mostramos en la pantalla
+            for(Habitaciones user : hab) {
+                System.out.println(user.getNum_hab() + " , "
+                    + user.getTipo_hab() + " , "
+                    +user.getPiso());
+            }
+            
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- Proyecto 2/src/Functions/Habitaciones.java
+++ Proyecto 2/src/Functions/Habitaciones.java
@@ -9,5 +9,55 @@ package Functions;
  * @author Maria Daniela
  */
 public class Habitaciones {
-    
+    private String num_hab; 
+    private String tipo_hab; 
+    private String piso; 
+
+    public Habitaciones(String num_hab, String tipo_hab, String piso) {
+        this.num_hab = num_hab;
+        this.tipo_hab = tipo_hab;
+        this.piso = piso;
+    }
+
+    /**
+     * @return the num_hab
+     */
+    public String getNum_hab() {
+        return num_hab;
+    }
+
+    /**
+     * @param num_hab the num_hab to set
+     */
+    public void setNum_hab(String num_hab) {
+        this.num_hab = num_hab;
+    }
+
+    /**
+     * @return the tipo_hab
+     */
+    public String getTipo_hab() {
+        return tipo_hab;
+    }
+
+    /**
+     * @param tipo_hab the tipo_hab to set
+     */
+    public void setTipo_hab(String tipo_hab) {
+        this.tipo_hab = tipo_hab;
+    }
+
+    /**
+     * @return the piso
+     */
+    public String getPiso() {
+        return piso;
+    }
+
+    /**
+     * @param piso the piso to set
+     */
+    public void setPiso(String piso) {
+        this.piso = piso;
+    }
 }
