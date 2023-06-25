fdbaa81617ac345356311c22043a8ef8154aa720
anabellajm <j.anabella@correo.unimet.edu.ve>
2023-06-25 16:46:06 -0400

    Menu principal

--- Proyecto 2/src/proyecto/pkg2/Main.java
+++ Proyecto 2/src/proyecto/pkg2/Main.java
@@ -4,6 +4,9 @@
  */
 package proyecto.pkg2;
 
+import GUIs.MainMenu;
+
+
 /**
  *
  * @author Anabella Jaua
@@ -15,6 +18,10 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        MainMenu menu = new MainMenu();
+        menu.setVisible(true);
+        menu.setLocationRelativeTo(null);
+        
     }
     
 }
